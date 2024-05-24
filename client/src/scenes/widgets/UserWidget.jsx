d6fd0799b4b7d0d4fe43347fe2c58f890450acb7
Saksham Kumar <sakshamkumar5457@gmail.com>
2024-05-24 15:22:23 +0530

    thirdcommit

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -22,7 +22,7 @@ const UserWidget = ({ userId, picturePath }) => {
   const main = palette.neutral.main;
 
   const getUser = async () => {
-    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+    const response = await fetch(`${window.location.origin}/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
