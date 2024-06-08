5fa3d75f168dd40712409d459ade9e93860796b0
Saksham Kumar <127621931+skmahto101210@users.noreply.github.com>
2024-06-08 14:12:42 +0530

    Update AdvertWidget.jsx

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src={`https://social-media-app-2-2wuk.onrender.com/assets/info4.jpeg`}
+        src={`https://social-media-app-server-gylg.onrender.com/assets/info4.jpeg`}
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
