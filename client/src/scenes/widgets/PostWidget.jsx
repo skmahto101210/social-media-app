714939cd576f7aa02928e99c89ee6ac8aa99e656
Saksham Kumar <127621931+skmahto101210@users.noreply.github.com>
2024-06-08 14:13:52 +0530

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -35,7 +35,7 @@ const PostWidget = ({
   const primary = palette.primary.main;
 
   const patchLike = async () => {
-    const response = await fetch(`https://social-media-app-2-2wuk.onrender.com/posts/${postId}/like`, {
+    const response = await fetch(`https://social-media-app-server-gylg.onrender.com/posts/${postId}/like`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -64,7 +64,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`https://social-media-app-2-2wuk.onrender.com/assets/${picturePath}`}
+          src={`https://social-media-app-server-gylg.onrender.com/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
