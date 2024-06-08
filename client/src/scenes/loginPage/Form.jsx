bd329812b8db171754b3a432a942900c0aa59d34
Saksham Kumar <127621931+skmahto101210@users.noreply.github.com>
2024-06-08 14:11:43 +0530

    Update Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      `https://social-media-app-2-2wuk.onrender.com/auth/register`,
+      `https://social-media-app-server-gylg.onrender.com/auth/register`,
       {
         method: "POST",
         body: formData,
@@ -80,7 +80,7 @@ const Form = () => {
 
   const login = async (values, onSubmitProps) => {
     const loggedInResponse = await fetch(
-      `https://social-media-app-2-2wuk.onrender.com/auth/login`,
+      `https://social-media-app-server-gylg.onrender.com/auth/login`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
